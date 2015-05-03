32fb6c7d13cb7e55ff83b152ae4a9718f275b5ed
Carlos Tse <copperoxide@gmail.com>
2015-05-03 15:14:43 +0800

    fixed the animation of the alien: issue #2

--- Assets/CMSC5727_assets/scripts/Enemy.js
+++ Assets/CMSC5727_assets/scripts/Enemy.js
@@ -19,9 +19,13 @@ function Hit() {
 	
 	if (anim) {
 		//anim.SetTrigger(deadHash);
+		
+		// move up a little bit
 		var v : Vector3 = gameObject.transform.position;
-		gameObject.transform.position = new Vector3(v.x, v.y + 0.5f, v.z);
-		anim.Play("dead");
+		gameObject.transform.position = new Vector3(v.x, v.y + 0.25f, v.z);
+		anim.SetBool("dead", true);
+		
+		// disapper after 3.9s
 		Destroy(gameObject, 3.9f);
 	}
 }
