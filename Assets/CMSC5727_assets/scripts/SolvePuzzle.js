073f315262f4beb8f484e0af2ddfc6fde627edae
Mond Wan <mondwan.1015@gmail.com>
2015-05-09 16:21:31 +0800

    Implement the core for solving puzzle green
    --Overview
    1. Able to detect hit from digit board
    2. Able to link up hit event from digit board to SolvePuzzleGreen.js
    3. Refactor codes in SolvePuzzles*
    4. Add tags, layers for digit board

--- Assets/CMSC5727_assets/scripts/SolvePuzzle.js
+++ Assets/CMSC5727_assets/scripts/SolvePuzzle.js
@@ -2,15 +2,30 @@
 
 class SolvePuzzle extends MonoBehaviour 
 {
-    var puzzles : int;
-    var solved : int;
+	protected var puzzleName : String;
+    protected var puzzles : int;
+    protected var solved : int;
+    public var gameManager : GameManager;    
     
     function Start(){
     	solved = 0;
+    	this.puzzleName = "Puzzle";
     }
     
     function SolveOne(){
     	solved++;
     	Debug.Log("[SolvePuzzle] solved: " + solved + "/" + puzzles);
+    }
+    
+    // Returns whether this puzzle has been solved or not
+    function IsSolved() {
+    	return this.puzzles == this.solved;
+    }
+    
+    // Handle logics after solving puzzles
+    function Solved() {
+    	Debug.Log(String.Format("[{0}] Solved", this.puzzleName));
+    	
+    	// Client specific implementation
     }
 }
--- Assets/CMSC5727_assets/scripts/SolvePuzzleGreen.js
+++ Assets/CMSC5727_assets/scripts/SolvePuzzleGreen.js
@@ -4,57 +4,98 @@
 #pragma implicit
 #pragma downcast
 
-class SolvePuzzleGreen extends MonoBehaviour
+class SolvePuzzleGreen extends SolvePuzzle
 {
 	public var obstacle : GameObject;
+	public var digitBoard : GameObject;
 	public var torches : GameObject[];
-	public var digitTrigger : GameObject;
 	
 	public var soundSource : AudioSource;
 	public var correctSoundEffect : AudioClip;
 	public var incorrectSoundEffect : AudioClip;
+	
+	protected var answers : Array;
 
 	function Start () {
+		this.puzzleName = "PuzzleGreen";
+	
 		// Disable all the flames first
+		this.resetPuzzles();
+		
+		// Define number of puzzles
+		this.puzzles = this.torches.Length;
+		
+		// Initialize number of solved puzzles
+		this.solved = 0;
+		
+		// Define answers of this puzzles
+		this.answers = new Array();
+		this.answers.Push("five");
+		this.answers.Push("seven");
+		this.answers.Push("two");
+		this.answers.Push("seven");
+	}
+	
+	// Disable all the flames
+	function resetPuzzles() {
 		for (torch in this.torches) {
 			var flameTransform : GameObject = torch.transform.Find("flames").gameObject;
 			var flame : GameObject = flameTransform.gameObject;
 			flame.SetActive(false);
-		}
-	}
-
-	function Update () {
-		// The activity of obstacle indicate whether the puzzle has been solved or not
-		if (this.obstacle.activeInHierarchy) {		
-			if (this.isPuzzleSolved()) {
-				// Player solves the puzzle
-				this.clear();
-			}
-		}
+		}	
+		this.solved = 0;
 	}
 	
-	// Helper method checks whether this puzzle had been solved or not
-	function isPuzzleSolved() {
-		// Checkout whether all flames are enabled or not
-		var ret : boolean = true;
-
-		for (torch in this.torches) {
-			var flameTransform : GameObject = torch.transform.Find("flames").gameObject;
-			var flame : GameObject = flameTransform.gameObject;
-			if (!flame.activeInHierarchy) {
-				ret = false;
-				break;
-			}
+	// Helper method lights up a torch
+	function lightUpTorch(index : int) {
+		var torch : GameObject;
+		torch = this.torches[index];
+		var flameTransform : GameObject = torch.transform.Find("flames").gameObject;
+		var flame : GameObject = flameTransform.gameObject;
+		flame.SetActive(true);		
+	}
+	
+	// A entry for checking out whether player hit a correct answer or not
+	function checkAnswer (answer : String) {
+		var expect : String;
+		expect = this.answers[this.solved];
+		
+		if (expect.Equals(answer)) {
+			this.lightUpTorch(this.solved);
+			this.solved++;
+		} else {
+			this.solved = 0;
+			this.resetPuzzles();
 		}
 		
-		Debug.Log("isFlamesDisabled: " + ret.ToString());
-		
-		return ret;
+		if (this.IsSolved()) {
+			this.Solved();
+		}
 	}
 	
 	// Help method handles what will be done after player solving the puzzle
-	function clear() {
+	function Solved() {
+		super.Solved();
+		
 		// Destroy the obstacle
 		this.obstacle.SetActive(false);
+		
+		// Play sound, ignite digitboard and destroy digitBoard
+		var flames : GameObject;
+		flames = this.digitBoard.transform.Find("flames").gameObject;
+		Debug.Log(flames.ToString);
+		flames.SetActive(true);
+		this.soundSource.Play();
+		Destroy(this.digitBoard, this.soundSource.clip.length);
+				
+		// notify game manager puzzle solved
+		gameManager.PuzzleSolved("Something happen at the back, let's go back to check!");
+	}
+
+	// An API for digitTriggers to notify this script
+	function notify(digitName : String) {
+		if (!this.IsSolved()) {
+			this.checkAnswer(digitName);
+		}
 	}
 }
