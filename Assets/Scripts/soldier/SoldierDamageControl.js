70be4b81e531f57aae68c03c4c40e65c184201d7
Mond Wan <mondwan.1015@gmail.com>
2015-05-09 23:42:14 +0800

    Fix #12:
    --Overview
    1. two mask added.

--- Assets/Scripts/soldier/SoldierDamageControl.js
+++ Assets/Scripts/soldier/SoldierDamageControl.js
@@ -12,7 +12,10 @@ class SoldierDamageControl extends MonoBehaviour
 	public var dyingHealthPoint : int = 20;
 	public var hpBar : UI.Slider;
 	public var heartImage : UI.Image;
-	public var gameManager : GameManager; 
+	public var gameManager : GameManager;
+	public var heartMask : UI.Image;
+	public var deathMask : UI.Image;
+	protected var deathMaskAlpha : float;
 	
 	// HP recover
 	public var recover : int = 1;
@@ -24,27 +27,57 @@ class SoldierDamageControl extends MonoBehaviour
 	public var dyingSound : AudioClip;
 	
 	function Start(){
+		deathMaskAlpha = 0;
 		updatePlayerStatUI();
 	}
 	
 	function Update(){
 		// HP recover
-		if (currentHealthPoint < maxHealthPoint && nextRecover < Time.time){
-			currentHealthPoint = Mathf.Min(currentHealthPoint + recover, maxHealthPoint);
-			Debug.Log("[Player] Recover +" + recover + " = " + currentHealthPoint);
-			updatePlayerStatUI();
-			nextRecover = Time.time + recoverTime;
+		if (!this.controller.dead) {
+			if (currentHealthPoint < maxHealthPoint && nextRecover < Time.time) {
+				currentHealthPoint = Mathf.Min(currentHealthPoint + recover, maxHealthPoint);
+				Debug.Log("[Player] Recover +" + recover + " = " + currentHealthPoint);			
+				nextRecover = Time.time + recoverTime;
+			}
+		} else {
+			this.deathMaskAlpha += Time.deltaTime;
+			this.deathMaskAlpha = this.deathMaskAlpha > 1 ? 1 : this.deathMaskAlpha;
 		}
+		
+		updatePlayerStatUI();
 	}
 	
 	// Helper method updates player's hp
 	function updatePlayerStatUI() {
 		hpBar.value = currentHealthPoint;
+		
+		if (!this.controller.dead) {
+			// Interpolate alphaRate by using remaining hpPercent
+			var hpPercent : float;
+			hpPercent= parseFloat(this.currentHealthPoint) / this.maxHealthPoint;
+			
+			var alphaValue : float;
+			alphaValue = 1 - hpPercent;
+			this.heartMask.color.a = alphaValue;
+		} else {
+			this.deathMask.color.a = this.deathMaskAlpha;		
+		}
+	}
+	
+	// Wrap calls for killing player
+	function killPlayer() {
+		this.controller.dead = true;
+		this.soundSource.PlayOneShot(this.dyingSound, 1.0);
+
+		this.heartMask.color = Color.clear;
+
+		this.gameManager.GameEnd(false);		
 	}
 	
 	// Define API similars to Enemy.js
 	function Hit (power : int) {
-		currentHealthPoint -= power;
+		this.currentHealthPoint -= power;
+		this.currentHealthPoint = this.currentHealthPoint < 0 ? 0 : this.currentHealthPoint;
 		Debug.Log("[Player] Destruct -" + power + " = " + currentHealthPoint);
 
 		this.soundSource.PlayOneShot(this.hitSound, 1.0);
@@ -53,10 +86,7 @@ class SoldierDamageControl extends MonoBehaviour
 		if (currentHealthPoint <= 0){
 			Debug.Log("[Player] Die");
 
-			this.controller.dead = true;
-			this.soundSource.PlayOneShot(this.dyingSound, 1.0);
-
-			this.gameManager.GameEnd(false);
+			this.killPlayer();
 		}
 	}
 
