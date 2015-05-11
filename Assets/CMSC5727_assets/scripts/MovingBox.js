8f3efc3e6c2152a6187631cf64648a72627ab8e1
Carlos Tse <copperoxide@gmail.com>
2015-05-11 23:59:13 +0800

    moving box

---
+++ Assets/CMSC5727_assets/scripts/MovingBox.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+public var min : float = -38f;
+public var max : float = -1f;
+public var speed : float = 2f;
+private var moveRight : boolean = true;
+
+function Update () {
+	var s : float = speed;
+	
+	if (moveRight && transform.position.x > max)
+		moveRight = false;
+	else if (!moveRight && transform.position.x < min)
+		moveRight = true;
+	
+	// speed up
+	if (!moveRight && transform.position.x > -34) s *= 5.0f;
+	
+	Debug.Log("x: " + transform.position.x + ", moveRight: " + moveRight + ", speed: " + s);
+	transform.Translate((moveRight ? Vector3.right :Vector3.left) * Time.deltaTime * s);
+}
